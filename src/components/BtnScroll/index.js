22f37bbfc7333bfb26026cd0b314270458ba9db2
Thiago Alves <80608138+Thiagof2755@users.noreply.github.com>
2023-10-03 21:58:00 -0300

    upgrade 03/10

---
+++ src/components/BtnScroll/index.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './BtnScroll.module.css';
+import BtnIng from './img/down-arrow.png';
+
+const BtnScroll = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const scrollToVisibleAreaEnd = () => {
+    const windowHeight = window.innerHeight;
+    const scrollYStart = window.scrollY;
+    const scrollStep = 5; // Ajuste a velocidade aqui
+
+    const scrollDown = () => {
+      const currentScroll = window.scrollY;
+
+      if (currentScroll < scrollYStart + windowHeight) {
+        window.scrollTo(0, currentScroll + scrollStep);
+        requestAnimationFrame(scrollDown);
+      }
+    };
+
+    requestAnimationFrame(scrollDown);
+  };
+
+  return (
+    isVisible && (
+      <div className={styles.btnContainer} onClick={scrollToVisibleAreaEnd}>
+        <img src={BtnIng} alt="Scroll to End of Visible Area" className={styles.btn} />
+      </div>
+    )
+  );
+};
+
+export default BtnScroll;
--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -4,15 +4,20 @@ import Navbar from "../../components/Navbar/index";
 import SessionOne from "../../components/SessionOne/index";
 import SessionTwo from "../../components/SessionTwo/index";
 import Sessionthree from "../../components/Sessionthree/index";
+import styles from "./inicio.module.css";
+
 
 function Inicio() {
     return (
+        <div className={styles.container}>
         <>
             <Navbar />
             <SessionOne />
             <SessionTwo />
             <Sessionthree />
+            
         </>
+        </div>
     );
 }
 

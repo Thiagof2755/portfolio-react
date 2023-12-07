b04f7030012734ffad2c367bd1045a3e7deb7bc9
Thiago Alves <80608138+Thiagof2755@users.noreply.github.com>
2023-12-06 22:21:48 -0300

    responsiviti

--- src/index.js
+++ src/index.js
@@ -1,16 +1,69 @@
-// App.js
-import React from 'react';
+// index.js
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter as BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import AppRoutes from './routes';
 import ParallaxBackground from './components/ParallaxBackground';
+import Navbar from './components/Navbar/index';
+import SessionOne from './components/SessionOne/index';
+import SessionTwo from './components/SessionTwo/index';
+import Sessionthree from './components/Sessionthree/index';
+import SessionFour from './components/SessionFour/index';
+import styles from './pages/Inicio/inicio.module.css';
+import ScrollReveal from 'scrollreveal';
+
+const App = () => {
+  useEffect(() => {
+    const sr = ScrollReveal({ reset: true });
+
+    sr.reveal('.SessionOne', {
+      duration: 1000,
+      distance: '100px',
+      origin: 'bottom',
+    });
+    sr.reveal('.SessionTwo', { duration: 1500 });
+    sr.reveal('.Sessionthree', { duration: 2000, rotate: { x: 0, y: 80, z: 0 } });
+    sr.reveal('.SessionFour', {
+      duration: 2000,
+      distance: '100px',
+      origin: 'bottom',
+    });
+  }, []);
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className={styles.container}>
+                <Navbar />
+                <div className={`SessionOne ${styles.SessionOne}`}>
+                  <SessionOne />
+                </div>
+                <div className={`SessionTwo ${styles.SessionTwo}`}>
+                  <SessionTwo />
+                </div>
+                <div className={`Sessionthree ${styles.Sessionthree}`}>
+                  <Sessionthree />
+                </div>
+                <div className={`SessionFour ${styles.SessionFour}`}>
+                  <SessionFour />
+                </div>
+              </div>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+      <ParallaxBackground />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      <AppRoutes />
-      <ParallaxBackground />
-    </div>
+    <App />
   </React.StrictMode>
 );
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Inicio from "./pages/Inicio/index"
+// router.js
+import React from 'react';
+import { HashRouter as BrowserRouter, Routes, Route } from 'react-router-dom';
+import Inicio from './pages/Inicio/index';
+
 function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Inicio/>} />
+                <Route path="/" element={<Inicio />} />
             </Routes>
         </BrowserRouter>
     );
